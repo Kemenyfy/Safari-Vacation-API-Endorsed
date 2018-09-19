25c17e318b155f110fe8ce66740d751a78b47e4c
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-19 17:14:54 -0400

    First Commit

---
+++ SafariVacationApiEndorsedContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace safari-vacation-api-endorsed
+{
+    public partial class SafariVacationApiEndorsedContext : DbContext
+    {
+        public SafariVacationApiEndorsedContext()
+        {
+        }
+
+        public SafariVacationApiEndorsedContext(DbContextOptions<SafariVacationApiEndorsedContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost; Database=SafariVacationApiEndorsed");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
